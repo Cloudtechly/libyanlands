cb59138baf4d858fbf418094b812ff5450d16675
faragbuograra <farag_2516@limu.edu.ly>
2026-01-04 17:23:53 +0200

    git branch -M main

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
           <ProcessSection />
           <GlobalReachSection />
           <ParallaxImages />
-          <TestimonialsSection />
+ 
           <CertificationsSection />
         
         
